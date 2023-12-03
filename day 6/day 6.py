c8d6ecf2fd8ee493e0cb01d2aa8a1510df23aad4
Anders Nilsson <contact@mg.nilsn.se>
2023-12-03 20:46:00 +0100

    Day 6 solution

---
+++ day 6/day 6.py
@@ -0,0 +1,27 @@
+import re
+
+#
+# Process input
+#
+with open('day 6/input.txt', 'r') as file:
+    content = file.read()
+
+#
+# Puzzle 1
+#
+for i in range(len(content)-3):
+    sequence = content[i:i+4]
+
+    if len(set(sequence)) == 4:
+        print("Puzzle 1 solution is:", i+4)
+        break
+
+#
+# Puzzle 2
+#
+for i in range(len(content)-13):
+    sequence = content[i:i+14]
+
+    if len(set(sequence)) == 14:
+        print("Puzzle 2 solution is:", i+14)
+        break
